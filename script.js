e6994d44c3fc6418675f811d672f77a06c0264e1
Max <max.w1@hotmail.com>
2024-01-08 21:35:37 +0000

    More styling

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Password validation
 const password = document.getElementsById('password');
 const passwordConfirm = document.getElementById('confirm-password');
+
+password.addEventListener('click', () => {
+    
+});
+passwordConfirm.addEventListener('click', () => {
+
+});
+
+// how do I make it so this runs every time text is entered? event listener
+if(password.innerHTML !== passwordConfirm.innerHTML) {
+    // make password and currentpassword invalid
+}
